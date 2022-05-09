dff58a2aae0ac991f0bd5f128e4ea547bf0807ff
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-05-09 08:21:26 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let numero1: number = 4;
+let numero2: number = 3;
+let numero3: number = 10;
+
+if (numero1 > numero2 && numero1 > numero3) {
+  console.log("El numero " + numero1 + " es el mayor");
+} else if (numero2 > numero1 && numero2 > numero3) {
+  console.log("El numero " + numero2 + " es el mayor");
+} else {
+  console.log("El numero " + numero3 + " es el mayor");
+}
